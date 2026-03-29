942bb52433b46c152cda7ec3bd61d4b5c385eccc
Adrien Le Berre <adrien.le-berre@epita.fr>
2026-03-29 14:45:15 +0200

    Ajout du site web

---
+++ docs/connexion/connexion.php
@@ -0,0 +1,16 @@
+<?php
+// Récupération des données
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+// exemple de connexion
+$correct_user = "admin";
+$correct_pass = "1234";
+
+if ($username === $correct_user && $password === $correct_pass) {
+    header("Location: accueil.html");
+    exit();
+} else {
+    echo "Nom d'utilisateur ou mot de passe incorrect.";
+}
+?>
