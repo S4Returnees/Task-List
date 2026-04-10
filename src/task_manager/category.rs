d38dd951b57430c3b4adf788849b8c045c7877eb
Alexandre <alexandre.dupuis@epita.fr>
2026-04-10 22:43:26 +0200

    init

---
+++ src/task_manager/category.rs
@@ -0,0 +1,22 @@
+pub struct Category 
+{
+    pub id: usize,
+    pub name: String,
+}
+
+impl Category
+{
+    pub fn new(id: usize, name: String) -> Self
+    {
+        Category
+        {
+            id,
+            name,
+        }
+    }
+
+    pub fn update_name(&mut self, name: String)
+    {
+        self.name = name;
+    } 
+}
---
+++ src/task_manager/category_list.rs
@@ -0,0 +1,31 @@
+use crate::task_manager::category::*;
+
+pub struct CategoryList
+{
+    list: Vec<Category>,
+}
+
+impl CategoryList
+{
+    pub fn new() -> Self
+    {
+        CategoryList
+        {
+            list: Vec::new(),
+        }
+    }
+
+    pub fn add(&mut self, mut new_category: Category)
+    {
+        let mut category_id = 0;
+        for category in self.list.iter()
+        {
+            if category.id == category_id
+            {
+                category_id += 1;
+            }
+        }
+        new_category.id = category_id;
+        self.list.push(new_category);
+    }
+}
