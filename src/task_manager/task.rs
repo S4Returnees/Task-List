abebca7cc5d6fb39070e81366061536717d96048
Starlight <linjerome95@gmail.com>
2026-04-10 13:08:42 +0200

    correction taskmanager

---
+++ src/task_manager/task.rs
@@ -0,0 +1,67 @@
+use chrono::NaiveDate;
+
+pub enum Priority {
+    Optional,
+    Low,
+    Medium,
+    High,
+    Critical,
+}
+
+pub enum Status {
+    Pending,
+    InProgress,
+    Done,
+}
+
+pub struct Task {
+    pub id: usize,
+    pub name: String,
+    pub description: String,
+    pub category_id: Option<usize>,
+    pub priority: Option<Priority>,
+    pub status: Option<Status>,
+    pub due_date: Option<NaiveDate>,
+}
+
+impl Task {
+    pub fn new(
+        name: String,
+        description: String,
+        category_id: Option<usize>,
+        priority: Option<Priority>,
+        due_date: Option<NaiveDate>,
+    ) -> Self {
+        Task {
+            id: 0,
+            name,
+            description,
+            category_id,
+            priority,
+            status: Some(Status::Pending),
+            due_date,
+        }
+    }
+
+    fn update_name(&mut self, name: String) {
+        self.name = name;
+    }
+    fn update_description(&mut self, description: String) {
+        self.description = description;
+    }
+
+    fn update_category_id(&mut self, category_id: Option<usize>) {
+        self.category_id = category_id;
+    }
+
+    fn update_priority(&mut self, priority: Option<Priority>) {
+        self.priority = priority;
+    }
+    fn update_status(&mut self, status: Option<Status>) {
+        self.status = status;
+    }
+
+    fn update_due_date(&mut self, due_date: Option<NaiveDate>) {
+        self.due_date = due_date;
+    }
+}
