1947d128fdf14b379d46a0b36a741cb1a0a536a8
Sayloox <quent26bdt@gmail.com>
2026-04-20 18:20:47 +0200

    Add sorting functionality to task list

--- src/task_manager/task_list.rs
+++ src/task_manager/task_list.rs
@@ -19,4 +19,30 @@ impl TaskList {
         new_task.id = task_id;
         self.list.push(new_task);
     }
+    pub fn sort_by(&mut self, criteria: &str) {
+        match criteria {
+            "Name" => {
+                self.list.sort_by(|a, b| a.name.to_lowercase().cmp(&b.name.to_lowercase()));
+            }
+
+            "Priority" => {
+                self.list.sort_by(|a, b| a.priority.cmp(&b.priority));
+            }
+
+            "Due Date" => {
+                self.list.sort_by(|a, b| match (a.due_date, b.due_date) {
+                    (Some(d1), Some(d2)) => d1.cmp(&d2),
+                    (None, Some(_)) => std::cmp::Ordering::Greater,
+                    (Some(_), None) => std::cmp::Ordering::Less,
+                    _ => std::cmp::Ordering::Equal,
+                });
+            }
+
+            "Status" => {
+                self.list.sort_by(|a, b| a.status.cmp(&b.status));
+            }
+
+            _ => {}
+        }
+    }
 }
