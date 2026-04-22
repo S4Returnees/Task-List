8d419bf0250ca64cc208089b86ac2d2cfbf7d56e
Starlight <linjerome95@gmail.com>
2026-04-22 15:41:12 +0200

    rename & remove category button icon

--- src/view/category_tasks.rs
+++ src/view/category_tasks.rs
@@ -2,7 +2,7 @@ use crate::TaskPlanner;
 use crate::message::Message;
 
 use crate::view::tasks_view_utils::{add_task_button, show_task, sort_by_combo_box};
-use iced::widget::{Space, button, column, container, row, rule, text};
+use iced::widget::{Space, button, column, container, row, rule, svg, text};
 use iced::{Element, Length, alignment};
 
 pub fn view(state: &TaskPlanner, id: usize) -> Element<'_, Message> {
@@ -44,17 +44,18 @@ fn tab_title(state: &TaskPlanner, id: usize) -> Element<'_, Message> {
 
 fn remove_button(id: usize) -> Element<'static, Message> {
     button(
-        container(text("-").size(15)) // todo edit svg
-            .width(Length::Fill)
-            .height(Length::Fill)
-            .align_x(alignment::Horizontal::Left)
-            .align_y(alignment::Vertical::Center),
+        container(
+            svg(svg::Handle::from_path("assets/delete.svg"))
+                .width(20)
+                .height(20),
+        )
+        .center(Length::Fill),
     )
-    .width(30)
-    .height(30)
+    .width(40)
+    .height(40)
     .style(|theme, status| {
         let mut style = button::primary(theme, status);
-        style.border.radius = 15.0.into();
+        style.border.radius = 20.0.into();
         style
     })
     .into()
@@ -62,17 +63,18 @@ fn remove_button(id: usize) -> Element<'static, Message> {
 
 fn rename_button(id: usize) -> Element<'static, Message> {
     button(
-        container(text("~").size(15)) // todo delete svg
-            .width(Length::Fill)
-            .height(Length::Fill)
-            .align_x(alignment::Horizontal::Left)
-            .align_y(alignment::Vertical::Center),
+        container(
+            svg(svg::Handle::from_path("assets/rename.svg"))
+                .width(20)
+                .height(20),
+        )
+        .center(Length::Fill),
     )
-    .width(30)
-    .height(30)
+    .width(40)
+    .height(40)
     .style(|theme, status| {
         let mut style = button::primary(theme, status);
-        style.border.radius = 15.0.into();
+        style.border.radius = 20.0.into();
         style
     })
     .into()
