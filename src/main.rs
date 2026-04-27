7a65df5a8a1a046d9ee9dd17df981ddfaac06fc2
Starlight <linjerome95@gmail.com>
2026-04-27 13:47:46 +0200

    add icon for app

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,29 @@ mod task_manager;
 mod view;
 
 use app::TaskPlanner;
+use iced::window::icon;
 use iced::{Size, Theme, window};
 
 pub fn main() -> iced::Result {
+    let icon = load_icon();
     iced::application(TaskPlanner::default, TaskPlanner::update, TaskPlanner::view)
         .title(|_state: &TaskPlanner| String::from("Task List"))
         .theme(|_state: &TaskPlanner| Theme::Dark)
         .window(window::Settings {
             //maximized: true,
             size: Size::new(1280.0, 720.0),
+            icon,
             ..Default::default()
         })
         .run()
+}
+
+fn load_icon() -> Option<icon::Icon> {
+    let bytes = include_bytes!("../assets/icon.png");
+
+    let img = image::load_from_memory(bytes).ok()?.to_rgba8();
+    let (width, height) = img.dimensions();
+    let rgba = img.into_raw();
+
+    icon::from_rgba(rgba, width, height).ok()
 }
