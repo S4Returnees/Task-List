804b49889624c9ca57aa915440d3f525f6cd8008
Starlight <linjerome95@gmail.com>
2026-04-27 14:04:32 +0200

    executable icon for windows

---
+++ build.rs
@@ -0,0 +1,10 @@
+﻿fn main() {
+    // Ce code ne s'exécute que si on compile pour Windows
+    #[cfg(target_os = "windows")]
+    {
+        let mut res = winres::WindowsResource::new();
+        // Assurez-vous que le chemin vers le fichier .ico est correct
+        res.set_icon("assets/icon.ico");
+        res.compile().unwrap();
+    }
+}
